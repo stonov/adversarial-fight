3c946dee59eecf2217dcb3d403596424e243ef4f
stonov <60677967+stonov@users.noreply.github.com>
2020-12-25 14:31:16 -0600

    spaceship can now shoot

---
+++ adversarial fight/Assets/Laser/LaserController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public int parent;
+    float speed = 10.0f;
+    // void Start(){
+    // }
+
+    void destroySelf(){
+        Destroy(gameObject);
+        Destroy(this);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0.0f, 0.0f));
+        Vector3 currentPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if(currentPosition.x > Screen.width + 64.0f ||
+            currentPosition.x < -64.0f ||
+            currentPosition.y > Screen.height + 64.0f ||
+            currentPosition.y < -64.0f){
+
+            destroySelf();
+        }
+        Debug.Log("x: " + currentPosition.x + " y: " + currentPosition.y);
+    }
+}
--- adversarial fight/Assets/Player/PlayerControls.cs
+++ adversarial fight/Assets/Player/PlayerControls.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour{
-    public bool is_player = true;
-    public Vector3 target_pos;
-    Quaternion current_rotation;
+    public bool isPlayer = true;
+    public Vector3 targetPos;
+    public GameObject laserPrefab;
+
+    Quaternion currentRotation;
+
     void Start(){
-        Quaternion current_rotation = transform.rotation;
+        Quaternion currentRotation = transform.rotation;
     }
     
     int ToInt(bool val){
@@ -15,20 +18,20 @@ public class PlayerControls : MonoBehaviour{
     }
     
     void updateOrientation(){
-        if(is_player){
-            target_pos = Input.mousePosition;
+        if(isPlayer){
+            targetPos = Input.mousePosition;
         }
-        var object_pos = Camera.main.WorldToScreenPoint(transform.position);
-        var target_angle = Mathf.Atan2(target_pos.y - object_pos.y, target_pos.x - object_pos.x) * Mathf.Rad2Deg;
+        var objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        var targetAngle = Mathf.Atan2(targetPos.y - objectPos.y, targetPos.x - objectPos.x) * Mathf.Rad2Deg;
         
-        var current_quat = current_rotation;
-        var goal_quat = Quaternion.Euler(new Vector3(0, 0, target_angle));
-        var trans_angle = Quaternion.Angle(current_quat, goal_quat);
-        var turn_speed = Time.deltaTime * 5.0f;
+        var currentQuat = currentRotation;
+        var goalQuat = Quaternion.Euler(new Vector3(0, 0, targetAngle));
+        var transAngle = Quaternion.Angle(currentQuat, goalQuat);
+        var turnSpeed = 30.0f;
         
-        var new_rotation = Quaternion.RotateTowards(current_quat, goal_quat, trans_angle * turn_speed);
-        transform.rotation = new_rotation;
-        current_rotation = transform.rotation;
+        var newRotation = Quaternion.RotateTowards(currentQuat, goalQuat, transAngle * turnSpeed * Time.deltaTime);
+        transform.rotation = newRotation;
+        currentRotation = transform.rotation;
     }
 
     int[] getInputFromArrowKeys(){
@@ -36,47 +39,72 @@ public class PlayerControls : MonoBehaviour{
         int downPressed = ToInt(Input.GetKey(KeyCode.S));
         int leftPressed = ToInt(Input.GetKey(KeyCode.A));
         int rightPressed = ToInt(Input.GetKey(KeyCode.D));
-        int[] input_dir = {upPressed, downPressed, leftPressed, rightPressed};
-        return input_dir;
+        int[] inputDir = {upPressed, downPressed, leftPressed, rightPressed};
+        return inputDir;
     }
 
     Vector3 getMoveDir(ref int shouldMove){
         // Up, Down, Left, Right
-        int[] input_dir = {0, 0, 0, 0};
-        if(is_player){
-            input_dir = getInputFromArrowKeys();
+        int[] inputDir = {0, 0, 0, 0};
+        if(isPlayer){
+            inputDir = getInputFromArrowKeys();
         }
-        int vertical_dir = input_dir[0] - input_dir[1];
-        int horizontal_dir = input_dir[3] - input_dir[2];
+        int verticalDir = inputDir[0] - inputDir[1];
+        int horizontalDir = inputDir[3] - inputDir[2];
 
-        if(vertical_dir == 0 && horizontal_dir == 0){
+        // if the sum from all keys is zero, then we should not move
+        if(verticalDir == 0 && horizontalDir == 0){
             shouldMove = 0;
         }else{
             shouldMove = 1;
         }
 
-        float angle = Vector3.Angle(new Vector3(1.0f, 0.0f, 0.0f), new Vector3(horizontal_dir, vertical_dir, 0.0f))*Mathf.Deg2Rad;
-        if(vertical_dir < 0){
+        // gets the angle between a unit vector on the x-axis and the current direction vector
+        float angle = Vector3.Angle(new Vector3(1.0f, 0.0f, 0.0f), new Vector3(horizontalDir, verticalDir, 0.0f))*Mathf.Deg2Rad;
+
+        // the angle has to be inverted if the vertical direction is negative
+        if(verticalDir < 0){
             angle *= -1.0f;
         }
-        Debug.Log("Hello: " + angle*Mathf.Rad2Deg + " input_dir " + input_dir[0] + " "+ input_dir[1] + " "+ input_dir[2] + " "+ input_dir[3] + " ");
+
+        // Debug.Log("Hello: " + angle*Mathf.Rad2Deg + " inputDir " + inputDir[0] + " "+ inputDir[1] + " "+ inputDir[2] + " "+ inputDir[3] + " ");
         return new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0.0f);
     }
 
     void updatePosition(){
         int shouldMove = 1;
-        Vector3 move_dir = getMoveDir(ref shouldMove);
+        Vector3 moveDir = getMoveDir(ref shouldMove);
         if(shouldMove == 0){
             return;
         }
-        float speed = 2.0f * Time.deltaTime;
-        transform.Translate(move_dir * speed);
+        float speed = 2.0f;
+        transform.Translate(moveDir * speed * Time.deltaTime);
+    }
+
+    void fireLaser(){
+        GameObject newLaser = Instantiate(laserPrefab, transform.position, currentRotation);
+        LaserController newLaserScript = newLaser.GetComponent<LaserController>();
+        newLaserScript.parent = gameObject.GetInstanceID();
+    }
+
+    void shootHandler(){
+        if(isPlayer){
+            if(Input.GetMouseButtonDown(0)){
+                fireLaser();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update(){
+        // reset the rotation to independently re-position ship
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
         updatePosition();
+
+        // restore the rotation
         updateOrientation();
+
+        // shoot if conditions are met
+        shootHandler();
     }
 }
